0e3cfdeae49c7f1b03bc7185819131c20669de5f
Dennis Ogiermann <termi-official@users.noreply.github.com>
2018-02-06 17:17:36 +0100

    Finished day 2 tasks

--- day2.py
+++ day2.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
 
 
 chosenClasses = [0,13,20]
@@ -13,6 +14,7 @@ maxSampleSize = 900
 datapath = "/home/fuckery/Downloads/GTSRB_Final_Training_Images/GTSRB/Final_Training/Images"
 
 [imgs, labels, class_descs, sign_ids] = load_gtsrb_images(datapath, chosenClasses, maxSampleSize)
+labels = np.array(labels)
 # change type from float32 to uint8 and convert to grayscale afterwards
 greyscaleImages = np.array([cv.cvtColor(img.astype(np.uint8), cv.COLOR_RGB2GRAY) for img in imgs])
 
@@ -36,10 +38,36 @@ pcs = pca.transform(vectors)
 for i,currentLabel in enumerate(chosenClasses):
     plt.scatter(pcs[labels == currentLabel,0],pcs[labels == currentLabel,1],c=colors[i])
 
-#indices = np.arange(len(greyscaleImages))
+plt.show()
 
-#svc = SVC(decision_function_shape = 'ovo')
-#svc.fit(pcs, labels)
+indices = np.arange(len(greyscaleImages))
+np.random.shuffle(indices)
+middle = int(len(greyscaleImages)/2)
+trainingIndices = indices[0:middle]
+testIndices = indices[middle:len(greyscaleImages)]
 
+svc = SVC(decision_function_shape = 'ovo')
+#svc.fit(pcs[trainingIndices], labels[trainingIndices])
+svc.fit(vectors[trainingIndices], labels[trainingIndices])
 
+predictedLabels = svc.predict(vectors[testIndices])
+
+cnfmat = confusion_matrix(labels[testIndices], predictedLabels)
+print("confusion matrix:")
+print(cnfmat)
+
+print("error rate:")
+print((cnfmat*np.array([[0,1,1],[1,0,1],[1,1,0]])).sum()/cnfmat.sum())
+
+misclassifiedIndices = labels[testIndices] != predictedLabels
+stuff = greyscaleImages[testIndices]
+misclassifiedImages = stuff[misclassifiedIndices]
+
+if len(misclassifiedImages):
+    index = np.random.randint(len(misclassifiedImages))
+    plt.imshow(misclassifiedImages[index],'gray')
+    print('classified as')
+    print(class_descs[predictedLabels[index]])
+    print('should be')
+    print(class_descs[labels[index]])
 plt.show()
