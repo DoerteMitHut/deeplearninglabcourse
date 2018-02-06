cff0c564f6c87b5e48a6bebd7ca23bae705d583e
Dennis Ogiermann <termi-official@users.noreply.github.com>
2018-02-07 00:19:04 +0100

    For the day 2 tasks: Added automatic coloring of scatter plot. Improced variable naming, readability and comments. Fixed a bug in the presentation of misclassified images.

--- day2.py
+++ day2.py
@@ -1,73 +1,122 @@
 from load_gtsrb import load_gtsrb_images
 
 import cv2 as cv
+
 import numpy as np
+
 import matplotlib.pyplot as plt
+
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
 
-
+# configuration
 chosenClasses = [0,13,20]
-colors = ["RED", "BLUE", "GREEN"]
-maxSampleSize = 900
+#chosenClasses = range(0,43)
+maxSampleSize = 500
 datapath = "/home/fuckery/Downloads/GTSRB_Final_Training_Images/GTSRB/Final_Training/Images"
 
-[imgs, labels, class_descs, sign_ids] = load_gtsrb_images(datapath, chosenClasses, maxSampleSize)
-labels = np.array(labels)
-# change type from float32 to uint8 and convert to grayscale afterwards
-greyscaleImages = np.array([cv.cvtColor(img.astype(np.uint8), cv.COLOR_RGB2GRAY) for img in imgs])
+maxNumMispredictedPreviews = 2
 
-winSize = (32,32)
+print("Loading data...")
+[images, labels, class_descs, sign_ids] = load_gtsrb_images(datapath, chosenClasses, maxSampleSize)
+# change type from float to uint8 and convert to grayscale afterwards
+greyscaleImages = np.array([cv.cvtColor(img.astype(np.uint8), cv.COLOR_RGB2GRAY) for img in images])
+
+# this configuration worked out quite nicely for hog feature extraction
+winSize = (32, 32) # use whole image, assume that the images all have the same size
 blockSize = (8,8)
 blockStride = (4,4)
 cellSize = (4,4)
-nbins = 8
+numBins = 8
 
+# how many cells do each block contain
+blocksPerCellHorizontal =  np.floor(blockSize[0]/cellSize[0])
+blocksPerCellVertical =  np.floor(blockSize[1]/cellSize[1])
+blocksPerCell =  int(blocksPerCellVertical*blocksPerCellHorizontal)
 
-hog = cv.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins)
+numCellsHorizontal = np.floor((winSize[0]-blockSize[0])/blockStride[0])+1
+numCellsVertical = np.floor((winSize[0]-blockSize[0])/blockStride[0])+1
+numCells = int(numCellsVertical*numCellsHorizontal)
 
-vectors = np.zeros((len(greyscaleImages),2*2*7*7*8))
+# calculate size of hog feature vector
+print("Computing feature vector...")
+hog = cv.HOGDescriptor(winSize, blockSize, blockStride, cellSize, numBins)
+vectors = np.zeros((len(greyscaleImages), numCells * blocksPerCell * numBins))
 for i, img in enumerate(greyscaleImages):
     vectors[i] = np.array(hog.compute(img)).flatten()
 
+# execute pricipal component analysis with 2 principal directions (lowering to a 2d subspace)
+print("Executing principal component analysis...")
 pca = PCA(n_components=2)
 pca.fit(vectors)
 pcs = pca.transform(vectors)
 
-for i,currentLabel in enumerate(chosenClasses):
-    plt.scatter(pcs[labels == currentLabel,0],pcs[labels == currentLabel,1],c=colors[i])
-
+# present labeled data
+for currentLabel in chosenClasses:
+    currentImages = pcs[labels == currentLabel, :]
+    plt.scatter(currentImages[:,0],currentImages[:,1], c=np.random.rand(3,), label=class_descs[currentLabel], alpha=0.3)
+plt.legend()
+plt.title('PCA')
 plt.show()
 
+# split set into test and training
+print("Generating test and training sets...")
 indices = np.arange(len(greyscaleImages))
 np.random.shuffle(indices)
 middle = int(len(greyscaleImages)/2)
-trainingIndices = indices[0:middle]
-testIndices = indices[middle:len(greyscaleImages)]
+trainingSetIndices = indices[0:middle]
+testSetIndices = indices[middle:len(greyscaleImages)]
 
+trainingSet = vectors[trainingSetIndices]
+trainingSetLabels = labels[trainingSetIndices]
+trainingSetImages = greyscaleImages[trainingSetIndices]
+
+testSet = vectors[testSetIndices]
+testSetLabels = labels[testSetIndices]
+testSetImages = greyscaleImages[testSetIndices]
+
+# train svm
+print("Train svm...")
 svc = SVC(decision_function_shape = 'ovo')
-#svc.fit(pcs[trainingIndices], labels[trainingIndices])
-svc.fit(vectors[trainingIndices], labels[trainingIndices])
+svc.fit(trainingSet, trainingSetLabels)
+testSetPredictions = greyscaleImages[testSetIndices]
 
-predictedLabels = svc.predict(vectors[testIndices])
+# test svm
+predictedLabels = svc.predict(testSet)
 
-cnfmat = confusion_matrix(labels[testIndices], predictedLabels)
+# generate confusion matrix
+cnfmat = confusion_matrix(testSetLabels, predictedLabels)
+# select non diagonal elements (which are the wrong classified ones)
+nonDiagonalMatrix = np.ones(cnfmat.shape)-np.diag(np.ones(cnfmat.shape[0]))
+cnfMatErrs = cnfmat*nonDiagonalMatrix
+# error rate is the number of wrong classified out of all classified entities
+errorRate = cnfMatErrs.sum()/cnfmat.sum()
+
 print("confusion matrix:")
 print(cnfmat)
+print("error rate: %f" % (errorRate))
 
-print("error rate:")
-print((cnfmat*np.array([[0,1,1],[1,0,1],[1,1,0]])).sum()/cnfmat.sum())
+# show an example of a misclassified image, if existing
+misclassifiedSelector = testSetLabels != predictedLabels
+if np.any(misclassifiedSelector):
+    # gather misclassification results
+    misclassifiedImages = testSetImages[misclassifiedSelector]
+    misclassifiedLabels = testSetLabels[misclassifiedSelector]
+    misclassifiedPredictions = predictedLabels[misclassifiedSelector]
+    misclassifiedByIndex = np.arange(len(misclassifiedLabels))
 
-misclassifiedIndices = labels[testIndices] != predictedLabels
-stuff = greyscaleImages[testIndices]
-misclassifiedImages = stuff[misclassifiedIndices]
+    # clamp number of presented mispredictions
+    maxNumMispredictedPreviews = np.clip(maxNumMispredictedPreviews, 1, len(misclassifiedLabels))
 
-if len(misclassifiedImages):
-    index = np.random.randint(len(misclassifiedImages))
-    plt.imshow(misclassifiedImages[index],'gray')
-    print('classified as')
-    print(class_descs[predictedLabels[index]])
-    print('should be')
-    print(class_descs[labels[index]])
-plt.show()
+    # select random image by index
+    np.random.shuffle(misclassifiedByIndex)
+    for index in misclassifiedByIndex[0:maxNumMispredictedPreviews]:
+        correctLabelName = class_descs[misclassifiedLabels[index]]
+        currentLabelName = class_descs[misclassifiedPredictions[index]]
+
+        plt.imshow(misclassifiedImages[index],'gray')
+        plt.title('Falsely classified as "%s"\n should be "%s"' % (currentLabelName, correctLabelName))
+        plt.show()
+else:
+    print('All images has been correctly classified')
