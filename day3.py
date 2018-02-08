a6f34fed169621459b87541715f3c5b550613351
Dennis Ogiermann <termi-official@users.noreply.github.com>
2018-02-08 11:27:48 +0100

    Improved network generation.

--- day3.py
+++ day3.py
@@ -9,11 +9,10 @@ import os
 # configuration
 logdir = os.path.dirname(os.path.realpath(__file__)) + '/log/'
 datapath = os.path.dirname(os.path.realpath(__file__)) + '/GTSRB/Final_Training/Images'
-chosenClasses = range(0, 10)
-#chosenClasses = range(0,43)
+chosenClasses = range(0, 5)
 numClasses = len(chosenClasses)
-maxSampleSize = 500
-batchSize = 15
+maxSampleSize = 1000
+batchSize = 100
 
 # reset stuff
 tf.reset_default_graph()
@@ -22,31 +21,56 @@ tf.reset_default_graph()
 print("Loading data...")
 [images, labels, class_descs, sign_ids] = load_gtsrb_images(datapath, chosenClasses, maxSampleSize)
 # change type from float to uint8 and convert to grayscale afterwards
-greyscaleImages = np.array([cv.cvtColor(img.astype(np.uint8), cv.COLOR_RGB2GRAY) for img in images])
+images = np.array([cv.cvtColor(img.astype(np.uint8), cv.COLOR_RGB2GRAY) for img in images])
+images = images.reshape([len(labels), 32, 32, 1])
 
 # split data into trainign, testing and verification
-X = tf.placeholder(tf.float32, [None, 32, 32])
+X = tf.placeholder(tf.float32, [None, 32, 32, 1])
 Y = tf.placeholder(tf.int32, [None])
+
+def buildConvLayer(input, convSize, convStride, poolSize, poolStride, inDepth, outDepth):
+    weights = tf.Variable(0.01*tf.random_normal([convSize,convSize,inDepth, outDepth]))
+    conv = tf.nn.conv2d(input, weights, strides=convStride, padding='SAME')
+    # add an bias to the
+    biased = tf.nn.bias_add(conv, tf.Variable(0.01*tf.random_normal([outDepth])))
+    # connect the biased convolutions to the rectified linear unit
+    relu = tf.nn.relu(biased)
+    # pool the relus output
+    return tf.nn.max_pool(relu, ksize=[1,poolSize,poolSize,1], strides=poolStride, padding='SAME')
+
+
+def buildFullyConnectedLayer(input, prevLayerSize, layerSize):
+    # build mapping
+    weights = tf.Variable(0.01*tf.random_normal([prevLayerSize,layerSize]))
+    # MAC the input, weights and biases
+    biased = tf.add(tf.matmul(input, weights), 0.01*tf.random_normal([layerSize]))
+    # apply rectified linear unit
+    return tf.nn.relu(biased)
+    #relu = tf.nn.relu(biased)
+    #
+    #dropout = tf.nn.dropout(relu, 0.8)
+    #return dropout
 
 # ------ build network with GPU support --------
 with tf.device('/device:GPU:0'):
-    xInput = tf.reshape(X, shape=[-1,32,32,1])
+    convStride = [1,1,1,1]
+    poolStride = [1,2,2,1]
 
-    # first convolution
-    weights = tf.Variable(tf.random_normal([5,5,1,32]))
-    conv = tf.nn.conv2d(xInput, weights, strides=[1,1,1,1], padding='SAME')
-    relu = tf.nn.relu(conv)
-    mpool = tf.nn.max_pool(relu, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+    # add three convolutions
+    first = buildConvLayer(X, 5, convStride, 2, poolStride, X.shape[3].value, 32)
+    second = buildConvLayer(first, 5, convStride, 2, poolStride, 32, 64)
+    lastConvLayer = buildConvLayer(second, 5, convStride, 2, poolStride, 64, 128)
 
-    # from convolution to neurons
-    reconnection = tf.reshape(mpool, [-1,16*16*32])
-    wd1 = tf.Variable(tf.random_normal([16*16*32,64]))
-    fcl = tf.matmul(reconnection, wd1)
+    # map from convolution to neurons
+    reconnection = tf.reshape(lastConvLayer, [-1,4*4*128])
 
-    # fully connected layer
-    outweights = tf.Variable(tf.random_normal([64, numClasses]))
-    fcl_relu = tf.nn.relu(fcl)
-    out = tf.matmul(fcl_relu, outweights)
+    # add two fully connected layers
+    fcl1 = buildFullyConnectedLayer(reconnection, 4*4*128, 2*1024)
+    fcl2 = buildFullyConnectedLayer(fcl1, 2*1024, 128)
+
+    # logits
+    outweights = tf.Variable(0.01*tf.random_normal([128, numClasses]))
+    out = tf.add(tf.matmul(fcl2, outweights), tf.Variable(0.01*tf.random_normal([numClasses])))
 
     # softmax
     pred = tf.nn.softmax(out)
@@ -58,7 +82,7 @@ saver = tf.train.Saver()
 # --------- optimizer ----------
 crossentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=out, labels=Y)
 loss_op = tf.reduce_mean(crossentropy)
-optimizer = tf.train.AdamOptimizer(learning_rate=0.005)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.00025    )
 train_op = optimizer.minimize(loss_op, global_step=tf.train.get_global_step())
 
 # ------- launch session -------
@@ -69,7 +93,7 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     # restore last session
-    #saver.restore(sess, tf.train.latest_checkpoint(logdir))
+    saver.restore(sess, tf.train.latest_checkpoint(logdir))
 
     # save graph
     train_writer = tf.summary.FileWriter(logdir+'/train', sess.graph)
@@ -77,29 +101,39 @@ with tf.Session() as sess:
 
     #build batches
     print("Generating test and training sets...")
-    indices = np.arange(len(greyscaleImages))
+    indices = np.arange(len(images))
     np.random.shuffle(indices)
 
     [trainingSetIndices, testSetIndices, validationSetIndices] = np.array_split(indices, 3)
     [trainingSetLabels, testSetLabels, validationSetLabels] = np.array_split(labels[indices], 3)
 
-    trainingSet = greyscaleImages[trainingSetIndices]
-    testSet = greyscaleImages[testSetIndices]
-    validationSet = greyscaleImages[validationSetIndices]
+    #[trainingSetIndices, testSetIndices] = np.split(indices, [int(len(indices)*75./100.)])
+    #[trainingSetLabels, testSetLabels] = np.split(labels[indices], [int(len(indices)*75./100.)])
 
-    print("Start training...")
+    trainingSet = images[trainingSetIndices]
+    testSet = images[testSetIndices]
+    validationSet = images[validationSetIndices]
+
     batchesX = np.split(trainingSet, np.arange(batchSize, len(trainingSet), batchSize))
     batchesY = np.split(trainingSetLabels, np.arange(batchSize, len(trainingSetLabels), batchSize))
+
+
     # execute optimization
-    for iter in range(0, len(batchesX)):
-        sess.run(train_op, feed_dict={X: batchesX[iter], Y: batchesY[iter]})
-        saver.save(sess, logdir, global_step=iter)
-        loss = sess.run(loss_op, feed_dict={X: batchesX[iter], Y: batchesY[iter]})
-        print("Loss: %f" % loss)
+    print("Start training...")
+    for epoch in range(0, 10):
+        for iter in range(0, len(batchesX)):
+            sess.run(train_op, feed_dict={X: batchesX[iter], Y: batchesY[iter]})
+            saver.save(sess, logdir, global_step=iter)
+
+            loss = sess.run(loss_op, feed_dict={X: trainingSet, Y: trainingSetLabels})
+            print("Test Loss: %f" % (loss))
+
+            loss = sess.run(loss_op, feed_dict={X: validationSet, Y: validationSetLabels})
+            print("Validation Loss: %f" % (loss))
 
     print("Optimization done!")
 
     # check performance
     result = pred.eval( {X: testSet[:256]} )
     predictions = tf.equal(tf.argmax(result, 1), tf.cast(testSetLabels[:256], tf.int64)).eval()
-    print("Test accuracy %s:" % (np.sum(predictions)/len(predictions)))
+    print("Test accuracy: %s" % (np.sum(predictions)/len(predictions)))
